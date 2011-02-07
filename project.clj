2a4e0c65198032170e093476800ec4d4c8ffead4
Scott Jaderholm <jaderholm@gmail.com>
2011-02-07 03:41:23 -0500

    Use my gaka branch
    So "foo,bar" doesn't duplicate styles for foo and bar.

--- project.clj
+++ project.clj
@@ -4,7 +4,7 @@
   :dependencies [[org.clojure/clojure "1.2.0"]
                  [org.clojure/clojure-contrib "1.2.0"]
                  [hiccup "0.3.1"]
-                 [gaka "0.2.0"]
+                 [org.clojars.scottjad/gaka "0.2.1"]
                  [org.clojars.scottjad/scriptjure "0.1.20"]
                  [uteal "0.0.1-SNAPSHOT"]]
   :dev-dependencies [[ring/ring-devel "0.3.3"]
