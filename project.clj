e31001c4d79469e914633efa0f26f713431ac8a7
Scott Jaderholm <jaderholm@gmail.com>
2011-02-10 00:52:24 -0500

    bump version 0.6.0

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject slice "0.5.0-SNAPSHOT"
+(defproject slice "0.6.0-SNAPSHOT"
   :description "Write composable slices of html, css, and js in Clojure"
   :url "http://github.com/scottjad/slice"
   :dependencies [[org.clojure/clojure "1.2.0"]
@@ -6,7 +6,7 @@
                  [hiccup "0.3.1"]
                  [org.clojars.scottjad/gaka "0.2.1"]
                  [org.clojars.scottjad/scriptjure "0.1.20"]
-                 [uteal "0.0.1-SNAPSHOT"]]
+                 [uteal "0.1.0-SNAPSHOT"]]
   :dev-dependencies [[ring/ring-devel "0.3.3"]
                      [ring/ring-jetty-adapter "0.3.3"]
                      [swank-clojure "1.3.0-SNAPSHOT"]
