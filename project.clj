ad3febb11307bff9ce9876b864a293d0729ad724
Scott Jaderholm <jaderholm@gmail.com>
2011-06-08 05:42:23 -0400

    use scriptjure with @ support

--- project.clj
+++ project.clj
@@ -5,7 +5,7 @@
                  [org.clojure/clojure-contrib "1.2.0"]
                  [hiccup "0.3.1"]
                  [org.clojars.scottjad/gaka "0.2.1"]
-                 [org.clojars.scottjad/scriptjure "0.1.20"]
+                 [org.clojars.scottjad/scriptjure "0.1.21"]
                  [uteal "0.1.0-SNAPSHOT"]]
   :dev-dependencies [[ring/ring-devel "0.3.3"]
                      [ring/ring-jetty-adapter "0.3.3"]
